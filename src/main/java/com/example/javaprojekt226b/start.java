25cea942e76914306974f1771492fe94f797a51f
marcschmidlin <marc.schmidlin@baloise.ch>
2022-05-16 15:14:47 +0200

    Create overviewgui and controller Create login gui and controller Create start

--- src/main/java/com/example/javaprojekt226b/start.java
+++ src/main/java/com/example/javaprojekt226b/start.java
@@ -1,7 +1,10 @@
 package com.example.javaprojekt226b;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +13,9 @@ import java.io.IOException;
 public class start extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(start.class.getResource("226bgui.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(start.class.getResource("logingui.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
+        stage.setTitle("Ausgaben Tracker");
         stage.setScene(scene);
         stage.show();
     }
