b30504d929faabd8730d3b49f8496de9cda97286
marcschmidlin <marc.schmidlin@baloise.ch>
2022-06-01 15:24:59 +0200

    Login gui Überarbeitung

--- src/main/java/ui/login/start.java
+++ src/main/java/ui/login/start.java
@@ -13,8 +13,8 @@ public class start extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("logingui.fxml"));
-        Scene scene = new Scene(root, 700, 400);
-        stage.setTitle("Ausgaben Tracker");
+        Scene scene = new Scene(root, 700, 500);
+        stage.setTitle("FEM");
         stage.setScene(scene);
         stage.show();
         stage.getIcons().add(new Image("/icon.png"));
