d8c50ec91db98d933ac01a455e2ca4a8e35d7880
Flavio <flavio.schaffer@bbzbl-it.ch>
2022-06-20 20:23:18 +0200

    test

--- src/main/java/entity/user.java
+++ src/main/java/entity/user.java
@@ -1,10 +1,32 @@
 package entity;
 
+import logic.loginlogic;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class user {
     private String firstname, lastname, username, email, password;
 
+    public void dateieinlesen(){
+        File datei = new File("src/main/java/entity/user.txt");
+        Scanner scan = null;
+        try {
+            scan = new Scanner(datei);
+        } catch (FileNotFoundException e){
+            System.out.println("File Not Found");
+        }
+        while (scan.hasNext()) {
+            firstname = scan.next();
+            lastname = scan.next();
+            username = scan.next();
+            email = scan.next();
+            password = scan.next();
+            logic.loginlogic loginlogic = new loginlogic(firstname, lastname, username, email, password);
+        }
 
-String minder;
+    }
+
+
 }
--- src/main/java/logic/loginlogic.java
+++ src/main/java/logic/loginlogic.java
@@ -5,10 +5,21 @@ import ui.login.logincontroller;
 import java.io.IOException;
 
 public class loginlogic {
+
+    private String firstname, lastname, username, email, password;
     public boolean schalter = false;
 
-    public boolean checkLogin1(String username, String password) throws IOException {
-        if (username.equals("hallo") && password.equals("123")) {
+    public loginlogic(String firstname, String lastname, String username, String email, String password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+
+    }
+
+    public boolean checkLogin1() throws IOException {
+        if (username == "Flxvio" && password =="cool") {
             return true;
         } else {
             return false;
