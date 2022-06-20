d785a1f0cc07591d29b7e836521e47ae347286e6
Flxvio1 <105214893+Flxvio1@users.noreply.github.com>
2022-06-20 16:08:56 +0200

    new login changes

--- src/main/java/logic/loginlogic.java
+++ src/main/java/logic/loginlogic.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 public class loginlogic {
     public boolean schalter = false;
 
-
     public boolean checkLogin1(String username, String password) throws IOException {
         if (username.equals("hallo") && password.equals("123")) {
             return true;
