180fbad60f7506467f64f2691ad6c647d3317f2e
Flxvio1 <105214893+Flxvio1@users.noreply.github.com>
2022-06-20 13:56:48 +0200

    first changes for login

--- src/main/java/logic/loginlogic.java
+++ src/main/java/logic/loginlogic.java
@@ -1,4 +1,10 @@
 package logic;
 
+import java.io.IOException;
+
 public class loginlogic {
+
+    public static void login() throws IOException {
+
+    }
 }
--- src/main/java/ui/login/logincontroller.java
+++ src/main/java/ui/login/logincontroller.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,15 +18,36 @@ public class logincontroller {
     private Label welcomeText;
     @FXML
     private Button login;
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
+
 
     @FXML
     public void LoginButtonPushed(ActionEvent event) throws IOException {
+       checkLogin(event);
+       // Parent tableViewParent = FXMLLoader.load(getClass().getResource("/ui/main/overview.fxml"));
+        //Scene tableViewScene = new Scene(tableViewParent, 1000, 600);
+        //Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        //window.setScene(tableViewScene);
+        //window.show();
+
+
+    }
+
+    private void checkLogin(ActionEvent event) throws IOException {
+        if (username.getText().toString().equals("javacoding") && password.getText().toString().equals("123")) {
         Parent tableViewParent = FXMLLoader.load(getClass().getResource("/ui/main/overview.fxml"));
         Scene tableViewScene = new Scene(tableViewParent, 1000, 600);
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(tableViewScene);
         window.show();
+            //m.changeScene("afterLogin.fxml");
+        }
     }
+
+
 
     @FXML
     public void Signuppushed(ActionEvent event) throws IOException {
